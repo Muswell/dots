f06f55a027e99f9e109671c0b28546a5420efb01
Shane Scanlon <slickrickjamesbrown@gmail.com>
2019-11-17 18:52:21 -0500

    force wlan to have value before starting polybar

--- i3/.i3/startup.sh
+++ i3/.i3/startup.sh
@@ -11,8 +11,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Store the wlan interface in an env variable
-export WLAN=$(ip link show | grep \<BROADCAST | awk -F':' '{print $2}' | xargs)
+# Sometimes the interface values don't populate immediately, so loop /shrug
+while [ -z "$WLAN" ]
+do
+  # Store the wlan interface in an env variable
+  export WLAN=$(ip link show | grep \<BROADCAST | awk -F':' '{print $2}' | xargs)
+  #export DEFAULT_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 for m in $(polybar --list-monitors | cut -d":" -f1); do
